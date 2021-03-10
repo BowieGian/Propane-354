d9078fde0d899553215fbbe31bc84b4ff7919e9d
BowieGian <43588714+BowieGian@users.noreply.github.com>
2021-03-09 19:46:57 -0800

    Added a basic login screen login button unfinished login.html extends from base.html base.html uses the main.css style updated the requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -10,9 +10,14 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     #~~~Finish Employee Columns Here~~~
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def index():
-    return "Hello World!"
+    if request.method == 'post':
+        username = request.form['username']
+        password = request.form['password']
+        return redirect('/')
+    else:
+        return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
