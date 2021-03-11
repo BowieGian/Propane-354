13388d9947e3d99669bd9eba0e7fd20d138ced99
BowieGian <43588714+BowieGian@users.noreply.github.com>
2021-03-10 16:32:31 -0800

    Changed password to id in login screen

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///propane354.db'
-db = SQLAlchemy(app)
-
-#class Employee(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    #~~~Finish Employee Columns Here~~~
 
 @app.route('/', methods=['get', 'post'])
 def index():
     if request.method == 'post':
         username = request.form['username']
-        password = request.form['password']
+        id = request.form['id']
         return redirect('/')
     else:
         return render_template('login.html')
