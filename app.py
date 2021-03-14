3f5791fac981be47e661a3c0579fa8a18f24f037
BowieGian <43588714+BowieGian@users.noreply.github.com>
2021-03-13 19:28:05 -0800

    Changed input for first name to a dropdown menu Renamed username to first name

--- app.py
+++ app.py
@@ -54,33 +54,39 @@ def insert_employee_qualification(connection, values):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    database = 'propane354.db'
+    connection = create_connection(database)
+
+    if (connection):
+        # insert an employee into the `employee` table  
+        employee1 = ('John', 'Doe', 'SomeSuffix', '2021-03-05', 42000, 'Laborer')
+        insert_employee(connection, employee1)
+        employee2 = ('Bob', 'Smith', 'Mr.', '2021-01-05', 40000, 'Laborer')
+        insert_employee(connection, employee2)
+
+        # insert a qualification into the `employee_qualification` table
+        employee1_qualifications = (1, 'Certified Inspector')
+        insert_employee_qualification(connection, employee1_qualifications)
+        employee2_qualifications = (2, 'Certified Inspector')
+        insert_employee_qualification(connection, employee2_qualifications)
+
+        cursor = connection.cursor()
+    else:
+        return "Failed to create database connection."
+
     if request.method == 'POST':
-        username = request.form['username']
+        first_name = request.form['first_name']
         id = request.form['id']
 
-        database = 'propane354.db'
-        connection = create_connection(database)
-    
-        if (connection):
-            # insert an employee into the `employee` table  
-            employee1 = ('John', 'Doe', 'SomeSuffix', '2021-03-05', 42000, 'Laborer')
-            insert_employee(connection, employee1)
-
-            # insert a qualification into the `employee_qualification` table
-            employee1_qualifications = (1, 'Certified Inspector')
-            insert_employee_qualification(connection, employee1_qualifications)
-
-            # find if login exists
-            cursor = connection.cursor()
-            if cursor.execute('SELECT id FROM employee WHERE first_name = ? AND id = ?', (username, id,)).fetchone():
-                return "Logged in!"
-            else:
-                return redirect('/')
+        # find if login exists
+        if cursor.execute('SELECT id FROM employee WHERE first_name = ? AND id = ?', (first_name, id,)).fetchone():
+            return "Logged in!"
         else:
-            return "Failed to create database connection."
+            return redirect('/')
 
     else:
-        return render_template('login.html')
+        employees = cursor.execute('SELECT first_name FROM employee').fetchall();
+        return render_template('login.html', employees=employees)
 
 if __name__ == "__main__":
     app.run(debug=True)
