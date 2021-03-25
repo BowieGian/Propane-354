5b20527ec36609bba04ecd2e58c915920d3e6ba3
Derek Lin <ys.lin113@gmail.com>
2021-03-24 19:54:21 -0700

    import create_connection function from our_sql.py, remove insert functions

--- app.py
+++ app.py
@@ -1,56 +1,11 @@
-from flask import Flask, render_template, url_for, request, redirect
 import pandas as pd
 import sqlite3
+
+from flask import Flask, render_template, url_for, request, redirect
+from our_sql import create_connection
 from sqlite3 import Error
 
 app = Flask(__name__)
-
-def create_connection(db_file):
-    # taken from https://www.sqlitetutorial.net/sqlite-python/create-tables/
-    
-    connection = None
-    try:
-        connection = sqlite3.connect(db_file)
-        return connection
-    except Error as e:
-        print(e)
-
-    return connection
-
-def insert(connection, sql, values):
-    # adapted https://www.sqlitetutorial.net/sqlite-python/insert/
-    
-    try:
-        cursor = connection.cursor()
-        cursor.execute(sql, values)
-        # connection.commit() # uncomment to commit changes to database
-    except Error as e:
-        print(e)
-
-def insert_employee(connection, values):
-    sql = '''
-        INSERT INTO employee(
-            first_name, 
-            last_name, 
-            suffix, 
-            start_date, 
-            salary, 
-            position
-        )
-        VALUES(?, ?, ?, ?, ?, ?);
-    '''
-    insert(connection, sql, values)
-
-    
-def insert_employee_qualification(connection, values):
-    sql = '''
-        INSERT INTO employee_qualification(
-            employee_id,
-            qualification
-        )
-        VALUES(?, ?);
-    '''
-    insert(connection, sql, values)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -58,18 +13,6 @@ def index():
     connection = create_connection(database)
 
     if (connection):
-        # insert an employee into the `employee` table  
-        employee1 = ('John', 'Doe', 'SomeSuffix', '2021-03-05', 42000, 'Laborer')
-        insert_employee(connection, employee1)
-        employee2 = ('Bob', 'Smith', 'Mr.', '2021-01-05', 40000, 'Laborer')
-        insert_employee(connection, employee2)
-
-        # insert a qualification into the `employee_qualification` table
-        employee1_qualifications = (1, 'Certified Inspector')
-        insert_employee_qualification(connection, employee1_qualifications)
-        employee2_qualifications = (2, 'Certified Inspector')
-        insert_employee_qualification(connection, employee2_qualifications)
-
         cursor = connection.cursor()
     else:
         return "Failed to create database connection."
